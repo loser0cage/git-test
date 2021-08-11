34cf700467ac41c50e470da8a9748fb0d0d4d6d7
xuyang <2863335816@qq.com>
2021-08-11 16:13:15 +0800

    eight commit

---
+++ src/main/java/com/xuyang/GitTest.java
@@ -0,0 +1,12 @@
+package com.xuyang;
+
+/**
+ * @author xu
+ * @Description
+ * @create 2021-08-11-16:01
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git,hello");
+    }
+}
